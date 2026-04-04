c6bfe195f604499d874731e69c5e2d9470d947c8
david-zhang-test <1839476290@qq.com>
2026-04-04 02:39:27 -0700

    update to 0.2.2

--- src/screens/me/SettingsScreen.tsx
+++ src/screens/me/SettingsScreen.tsx
@@ -43,7 +43,7 @@ const LANGUAGE_OPTIONS = [
   { value: 'en' as const, labelKey: 'english' },
 ];
 
-const APP_VERSION = '0.1.9';
+const APP_VERSION = '0.2.2';
 
 export default function SettingsScreen({ navigation }: Props) {
   const { t, i18n } = useTranslation();
